c7c91fe45309a1d4a6edff83db5332d87afef351
Vlad Marian <vmarian@mimecast.com>
2021-05-01 23:05:11 +0100

    Public

--- src/test/java/com/mimecast/robin/smtp/EmailDeliveryMock.java
+++ src/test/java/com/mimecast/robin/smtp/EmailDeliveryMock.java
@@ -2,7 +2,7 @@ package com.mimecast.robin.smtp;
 
 import com.mimecast.robin.smtp.connection.Connection;
 
-class EmailDeliveryMock extends EmailDelivery {
+public class EmailDeliveryMock extends EmailDelivery {
 
     public EmailDeliveryMock(Connection connection) {
         super(connection.getSession());
