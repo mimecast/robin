2fd85ca30e562f1c98adb40a6deb326da4b71076
Vlad Marian <vmarian@mimecast.com>
2021-05-01 22:58:41 +0100

    Add path based constructor

--- src/main/java/com/mimecast/robin/config/BasicConfig.java
+++ src/main/java/com/mimecast/robin/config/BasicConfig.java
@@ -1,11 +1,23 @@
 package com.mimecast.robin.config;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
  * Config generic implemetation.
  */
 public class BasicConfig extends ConfigFoundation {
+
+    /**
+     * Constructs a new BasicConfig instance with given path.
+     *
+     * @param path Configuration pathj.
+     * @throws IOException Unable to read file.
+     */
+    @SuppressWarnings("rawtypes")
+    public BasicConfig(String path) throws IOException {
+        super(path);
+    }
 
     /**
      * Constructs a new BasicConfig instance with given map.
